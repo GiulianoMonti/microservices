d7c961767dc82f6d0511e175e4a053795a81e0b3
giuliano monti <giulianm@gmail.com>
2022-02-27 14:15:35 -0300

    first

---
+++ customer/src/main/java/com/g/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,7 @@
+package com.g.customer;
+
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email) {
+}
---
+++ customer/src/main/java/com/g/customer/CustomerService.java
@@ -0,0 +1,39 @@
+//package com.g.customer;
+//
+//public class CustomerService {
+//
+//    private final CustomerRepository customerRepository;
+//    private final FraudClient fraudClient;
+//    private final RabbitMQMessageProducer rabbitMQMessageProducer;
+//
+//    public void registerCustomer(CustomerRegistrationRequest request) {
+//        Customer customer = Customer.builder()
+//                .firstName(request.firstName())
+//                .lastName(request.lastName())
+//                .email(request.email())
+//                .build();
+//        // todo: check if email valid
+//        // todo: check if email not taken
+//        customerRepository.saveAndFlush(customer);
+//
+//        FraudCheckResponse fraudCheckResponse =
+//                fraudClient.isFraudster(customer.getId());
+//
+//        if (fraudCheckResponse.isFraudster()) {
+//            throw new IllegalStateException("fraudster");
+//        }
+//
+//        NotificationRequest notificationRequest = new NotificationRequest(
+//                customer.getId(),
+//                customer.getEmail(),
+//                String.format("Hi %s, welcome to Amigoscode...",
+//                        customer.getFirstName())
+//        );
+//        rabbitMQMessageProducer.publish(
+//                notificationRequest,
+//                "internal.exchange",
+//                "internal.notification.routing-key"
+//        );
+//
+//    }
+//}
